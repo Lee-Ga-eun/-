97a397cd80e2940acf8a8de10005adcb886c79f7
Gaeun Lee <gaeun0800@gmail.com>
2022-06-28 14:03:22 +0900

    key= lambda x:x[n]

--- PYTHON Lv1/문자열 내 마음대로 정렬하기.py
+++ PYTHON Lv1/문자열 내 마음대로 정렬하기.py
@@ -15,3 +15,29 @@ def solution(strings, n):
     return answer
     
      ## 딕셔너리는 순서가 없다. 따라서 OrderedDict를 통해 순서를 정해줘야 한다
+     ## 키는 중복될 수 없다
+    
+# 풀이2
+
+def solution2(strings, n):
+    answer=sorted(sorted(strings), key=lambda x:x[n])
+    return answer
+
+
+#풀이3
+from operator import itemgetter, attrgetter, methodcaller
+
+def solution(strings, n):
+    return sorted(sorted(strings), key=itemgetter(n))
+
+
+### lambda
+# lambda는 anonymous function 이다
+f= lambda: 'foo'
+f # 결과: <function __main__.<lambda>()>
+f() # 결과: 'foo'
+
+def lowercased(word): return word.lower()
+sorted(['Some', 'words', 'sort', 'differently'], key=lowercased)
+
+sorted(['Some', 'words', 'sort','differently'], key=lambda x: x[3])
