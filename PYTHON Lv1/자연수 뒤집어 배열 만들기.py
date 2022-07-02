62e825212c58016403715054a55865e8e504b82d
Gaeun Lee <gaeun0800@gmail.com>
2022-07-02 16:33:24 +0900

    map(function, iterable) / reversed()는 iterator를 반환

--- PYTHON Lv1/자연수 뒤집어 배열 만들기.py
+++ PYTHON Lv1/자연수 뒤집어 배열 만들기.py
@@ -43,3 +43,28 @@ def solution(n):
     테스트 12 〉	통과 (0.01ms, 10.2MB)
     테스트 13 〉	통과 (0.01ms, 10.1MB)
     """
+
+# 풀이3
+def solution(n):
+    return list(map(int, reversed(str(n))))
+
+    # list(reversed(str(n))) ==> ['5','4','3','2','1']
+    # reversed(str(n)) ==> <reversed at 0x7fe5e9351430>
+    # reversed() 함수는 자료구조에 담긴 원소들을 역순으로 순회할 수 있도록 "iterator"를 결과값으로 반환한다
+
+    # map(function, iterable)
+
+
+# 풀이4
+
+def solution(n):
+    return [int(i) for i in str(n)][::-1]
+
+
+# 풀이 3 & 4
+
+def soltuion(n):
+    return list(map(int, list(str(n))[::-1]))
+
+    # list('1234') ==> ['1','2','3','4']
+    
