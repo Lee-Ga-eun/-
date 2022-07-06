0b18dbbb67e40ca021a8b76dd0dcf1836a22ff19
Gaeun Lee <gaeun0800@gmail.com>
2022-07-06 13:27:54 +0900

    return True if else False 구문

---
+++ PYTHON Lv1/하샤드수.py
@@ -0,0 +1,27 @@
+"""
+양의 정수 x가 하샤드 수라면, x의 자릿수의 합으로 x가 나누어져야 한다
+
+입력 10 ==> 출력 true
+입력 12 ==> 출력 true
+입력 11 ==> 출력 false
+
+"""
+# 풀이1
+def solution(x):
+    answer=0
+    for i in str(x):
+        answer+=int(i)
+    return x%answer==0 or False
+
+# 풀이2
+
+def solution(x):
+    return x%sum([int(i) for i in str(x)])==0
+
+#풀이3
+
+def solution(x):
+    answer=0
+    for i in str(x):
+        answer+=int(i)
+    return True if x%answer==0 else False
