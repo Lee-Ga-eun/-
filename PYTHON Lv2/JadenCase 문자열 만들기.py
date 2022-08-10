d0c0f8566da5cf387b196b2a00cc7d2603257f28
Gaeun Lee <gaeun0800@gmail.com>
2022-08-10 16:16:54 +0900

    .split()은 모든 공백을 제거한다

---
+++ PYTHON Lv2/JadenCase 문자열 만들기.py
@@ -0,0 +1,24 @@
+"""
+모든 단어의 첫 문자가 대문자이고 그 외의 알파벳은 소문자이다
+첫 문자가 숫자일 때는 그 뒤 이어지는 알파벳은 소문자로 출력한다
+공백 문자가 연속해서 나올 수 있다
+"""
+
+
+def solution(s):
+    s=s.lower()
+    s=s.lstrip()
+    s=s.split(" ")
+    answer=[]
+    for i in s:
+        if i=="":
+            answer.append("")
+            continue
+        if i[0].isdigit():
+            answer.append(i)
+        else:
+            k=chr(ord(i[0])-32)
+            i=k+i[1::]
+            answer.append(i)
+    
+    return " ".join(answer)
