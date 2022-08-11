5ba1fe82e10650a5c930f4fc52fbe5c215158b82
Gaeun Lee <gaeun0800@gmail.com>
2022-08-11 23:10:41 +0900

    Create 다리 위를 지나는 트럭.py

---
+++ PYTHON Lv2/다리 위를 지나는 트럭.py
@@ -0,0 +1,18 @@
+"""
+* 다리에는 트럭이 최대 bridge_length대 올라갈 수 있으며, 다리는 weight 이하까지의 무게를 견딜 수 있다.
+1초당 1 length 움직일 수 있고, 대기트럭에 있는 순서대로 움직인다
+
+<입력>
+bridge_length=2
+weight=10
+truck_weights=[7,4,5,6]
+<출력>
+return 8
+
+<입력>
+bridge_length=100
+weight=100
+truck_weights=[10]
+<출력>
+return 101
+"""
