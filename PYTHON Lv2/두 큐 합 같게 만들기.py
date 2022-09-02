3c6992b43b16f2ce0e8beea1005eaa59e36fdac7
Gaeun Lee <gaeun0800@gmail.com>
2022-09-02 12:05:52 +0900

    2022 카카오 tech internship

--- PYTHON Lv2/두 큐 합 같게 만들기.py
+++ PYTHON Lv2/두 큐 합 같게 만들기.py
@@ -1,4 +1,4 @@
-# 초안
+# 초안 -> Cancle
 
 from collections import deque
 def solution(queue1,queue2):
@@ -30,3 +30,30 @@ def solution(queue1,queue2):
     
     
     return 0
+
+
+# 더 쉽게 접근하기
+# 계속 sum을 해줘야한다고 생각했으나, 그렇지 않음
+
+from collections import deque
+
+def solution(queue1, queue2):
+    queue1=deque(queue1)
+    queue2=deque(queue2)
+    sum_queue1=sum(queue1)
+    sum_queue2=sum(queue2)
+
+    for i in range(len(queue1)*3): #queue2과 queue1의 길이는 동일하다. popleft와 append가 수행되므로, 넉넉하게 길이를 잡아준다
+        if sum_queue1==sum_queue2: # 합이 같으면
+            return i # 몇 번 수행되었는지 == popleft, popleft된 수의 개수
+        if sum_queue1>sum_queue2: #queue1의 합이 더 크면
+            num=queue1.popleft() # queue1에서 가장 앞 수를 빼서 queue2에 넣는다
+            queue2.append(num)
+            sum_queue1-=num # 굳이 모든 수를 더하지 않아도 된다
+            sum_queue2+=num
+        else: #queue2가 더 클 때
+            num=queue2.popleft()
+            queue1.append(num)
+            sum_queue1+=num
+            sum_queue2-=num
+    return -1 # 합이 도저히 같지 않을 때
